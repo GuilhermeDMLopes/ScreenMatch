f6ced62631b56660a861317da9b433613f9ca178
Guilherme <guilhermedmlopes@hotmail.com>
2024-09-19 16:32:12 -0300

    Relembrando conceitos de java

---
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -0,0 +1,87 @@
+package br.com.alura.screenmatch.modelos;
+
+import br.com.alura.screenmatch.calculos.Classificavel;
+
+//Usando Herança (extends)
+//Usando iterface (implements). Toda interface deve ser obrigatorio o uso de seus metodos
+public class Filme extends Titulo implements Classificavel {
+    /* Tudo isso será Herdade da classe titulo a partir de agora
+    //Atributos de filme (Caracteristica comum para todos)
+    //Encapsulamento (private). Tambem conhecido como modificador de acesso. Não permite que o usuario altere informações indevidas.
+    //Quando criamos instancias da classe em pacotes diferentes, preciso explicitar o que é publico, privado, etc.
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacoes;
+    private int duracaoEmMinutos;
+
+    //Metodo acessor para pegar valores de atributos privados
+    public int getTotalDeAvaliacoes() {
+        return totalDeAvaliacoes;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    //Métodos. Ações que todos os filmes fazem
+    public void exibeFichaTecnica() {
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+    }
+
+    public void avaliaFilme(double nota) {
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    public double pegaMedia() {
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+    }*/
+
+    //Atributos específicos de Filme
+    private String diretor;
+
+    //(Alt+fn+Insert) para getters e setters
+    public String getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
+
+    @Override
+    public int getClaffificacao() {
+        return (int) pegaMedia() / 2;
+    }
+}
---
+++ src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -0,0 +1,65 @@
+package br.com.alura.screenmatch.modelos;
+
+//Iremos utilizar o conceito de Herança. Serie e Filme possuem os atributos de titulo
+//Série é um titulo, Filme é um titulo e por aí vai.
+public class Titulo {
+
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacoes;
+    private int duracaoEmMinutos;
+
+    //Metodo acessor para pegar valores de atributos privados
+    public int getTotalDeAvaliacoes() {
+        return totalDeAvaliacoes;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    //Métodos. Ações que todos os filmes fazem
+    public void exibeFichaTecnica() {
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+    }
+
+    public void avaliaFilme(double nota) {
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    public double pegaMedia() {
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
+}
